fff2e7e5206c43fc13bdf9b6049b5099dc56e6df
Rindi Indriani <rindi.hafizh@gmail.com>
2024-05-23 14:02:50 +0700

    header huffmanTree with line border dan menghilangkan huffmanTree no line

--- Implementasi Huffman Code CPP/Rindi.h
+++ Implementasi Huffman Code CPP/Rindi.h
@@ -6,7 +6,10 @@
 
 // Function declarations for Huffman Tree creation and code generation
 nAddress buildHuffmanTree(qAddress* head);
-void printBinaryTree(node* root, int space, int height);
+void printNodeValue(node* n);
+void printTree(node* root, bool isRight, const char* indent);
+void printBinaryTree(node* root);
+//void printBinaryTree(node* root, int space, int height);
 void generateCodes(nAddress root, char* code, int top, table* codeTable);
 void printCodes(tAddress codeTable);
 
